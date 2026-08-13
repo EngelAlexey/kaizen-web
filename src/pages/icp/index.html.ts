03311602e12af79179f5db493cf0a7b823711174
Kaizen Software <desarrollo@kaizenapps.net>
2026-08-13 11:21:27 -0600

    Update index.html.ts

--- src/pages/icp/index.html.ts
+++ src/pages/icp/index.html.ts
@@ -2,5 +2,5 @@ import { GET_iframe } from "../../lib/iframePage";
 
 export const GET = GET_iframe(
     "Kaizen CXC",
-    "https://script.google.com/a/macros/kaizenapps.net/s/AKfycbxG_dY2ZmMkZTejOSMTo10xsWdgyRto2PGyie9qTN1zsn4Pw6HugSdGmw5IK_B7F1nZww/exec"
+    "https://script.google.com/macros/s/AKfycbzBY3uXGSYGswIwhinIzNjurfcff9gAQQ1z28jvg8VXB6jPoi_iGLvYu_635jDBQj63bw/exec"
 );
